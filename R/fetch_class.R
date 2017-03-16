d7652e5d1f7f7034e16bccd0fea3f7d8a2a519ee
Blake Seers <blake.seers@gmail.com>
2017-03-16 07:44:13 +0000

    version 2.0-0

---
+++ R/fetch_class.R
@@ -0,0 +1,290 @@
+#' Calculate Wind Exposure with the \pkg{fetchR} Package
+#' 
+#' The \pkg{fetchR} package allows for an objective calculation of wind fetch 
+#' and provides methods to visualise the wind exposure and export the fetch 
+#' vectors to a KML file.
+#' 
+#' Fetch is an important measurement in coastal applications. It
+#' provides a measurement for the unobstructed length of water that wind from a
+#' certain direction can blow over. The higher the wind fetch from a certain 
+#' direction, the more energy is imparted onto the surface of the 
+#' water resulting in a larger sea state. Therefore, the larger
+#' the fetch, the larger the exposure to wind and the more likely the
+#' site experiences larger sea states.
+#' 
+#' The fetch length from all directions (and from each quadrant) can 
+#' be averaged to provide an indication of the location's exposure to 
+#' wind. The \pkg{fetchR} package 
+#' calculates the lengths of wind fetch vectors from all directions, at any 
+#' given location(s) on Earth, and can provide summaries, visualisations and KML 
+#' files along with the raw data.
+#'  
+#' @seealso \code{\link{fetch}} for an extensive reproducible example.
+#' @seealso \code{vignette("introduction-to-fetchR")} for a short introduction 
+#' to **fetchR**.
+#' @name fetchR
+#' @docType package
+#' @keywords package
+NULL
+
+#' Fetch Class
+#' 
+#' Class to hold Fetch objects. 
+#' 
+#' A \code{Fetch} object is essentially a list of \code{\link[sp]{SpatialLines}}
+#' objects.
+#' 
+#' @note Fetch objects should only be created using the \code{\link{fetch}} 
+#' function.
+#' 
+#' @slot names character vector containing the names for each location.
+#' @slot max_dist numeric vector of length 1 containing the maximum distance
+#'                a fetch vector is allowed.
+#'                
+#' @section Extends:
+#' Class \code{"list"} directly, and class \code{"vector"}, by class \code{"list"}.
+#' 
+#' @name Fetch
+#' @rdname Fetch-class
+#' @importFrom methods setClass
+setClass("Fetch", slots = c(names = "character", max_dist = "numeric"), 
+         contains = "list")
+
+#' @importFrom sp proj4string
+# Fetch Class Validation
+# 
+# Checks to ensure that all the CRS are equal.
+valid_fetch = function(object){
+  errors = character()
+  
+  if (length(unique(sapply(object, proj4string))) != 1){
+    msg = "All sites must have the same CRS"
+    errors = c(errors, msg)
+  }
+  
+  if (length(errors) == 0){
+    TRUE
+  } else {
+    errors
+  }
+}
+
+#' @importFrom methods setValidity
+setValidity("Fetch", valid_fetch)
+
+#' Summarise a Fetch Object
+#' 
+#' The \code{summary} function calculates the average fetch for the separate 
+#' northerly, easterly, southerly and westerly quadrants. For example, the mean 
+#' fetch for the northerly component averages over the fetch vectors with 
+#' directions between 315 (inclusive) and 45 (exclusive) degrees, i.e. the fetch 
+#' vectors within the interval [315, 45).
+#' 
+#' @return The \code{summary} function returns a \code{\link{data.frame}}.
+#' 
+#' @param object a \code{Fetch} object that has been returned by the 
+#'               \code{\link{fetch}} function.
+#' 
+#' @importFrom methods setMethod
+#' @export
+setMethod("summary", "Fetch", function(object){
+  summary.df = data.frame(t(sapply(object, function(x){
+    by(x@data$fetch, x@data$quadrant, mean)
+  })))
+  summary.df$Average = sapply(object, function(x) 
+    mean(x@data$fetch))
+  summary.df
+})
+
+#' @rdname summary-Fetch-method
+#' @importFrom methods setMethod
+#' @importFrom methods show
+setMethod("show", "Fetch", function(object){
+
+  cat("Is projected\t: ", is.projected(object[[1]]), "\n",
+      "Max distance\t: ", object@max_dist, " km\n",
+      "Directions\t: ", nrow(object[[1]]@data), "\n",
+      "Sites\t\t: ", length(object), "\n\n",
+      sep = "")
+  print(round(summary(object), 1))
+})
+
+#' spTransform for map projection and datum transformation
+#' 
+#' spTransform for map projection and datum transformation
+#' 
+#' @param x \code{\link{Fetch}} object to be transformed
+#' @param CRSobj object of class \code{\link[sp]{CRS}}, or of class character in
+#'               which case it is converted to \code{\link{CRS}}
+#' @param ... further arguments (ignored)
+#' @importFrom methods setMethod new
+#' @importFrom sp spTransform CRS
+#' @return \code{\link{Fetch}} object with coordinates transformed to the 
+#'         new coordinate system.
+#' @export
+setMethod("spTransform", 
+          signature(x = "Fetch", CRSobj = "CRS"), 
+          function(x, CRSobj){
+            validObject(x)
+            obj_new = lapply(x, spTransform, CRSobj)
+            new("Fetch", obj_new, names = x@names, max_dist = x@max_dist)
+          }
+)
+
+#' @rdname spTransform-Fetch-CRS-method
+#' @importFrom methods setMethod
+#' @importFrom sp spTransform CRS
+#' @export
+setMethod("spTransform", 
+          signature(x = "Fetch", CRSobj = "character"), 
+          function(x, CRSobj){
+            validObject(x)
+            CRSobj = CRS(CRSobj)
+            spTransform(x, CRSobj)
+            }
+)
+
+#' Retrieve projection attributes for Fetch objects
+#' 
+#' Retrieve projection attributes for \code{\link{Fetch}} objects.
+#' @param obj \code{\link{Fetch}} object
+#' @importFrom methods setMethod signature
+#' @importFrom sp proj4string
+#' @export
+setMethod("proj4string",
+          signature(obj = "Fetch"),
+          function (obj) {
+            validObject(obj)
+            
+            proj4string(obj[[1]])
+          }
+)
+
+#' @importFrom methods setAs
+#' @importFrom sp is.projected
+setAs("Fetch", "data.frame", function(from){
+  validObject(from)
+  fetch.df = data.frame(do.call("rbind", lapply(from, slot, "data")), 
+                        row.names = NULL)
+  
+  coords.mat = do.call("rbind", 
+                       lapply(
+                         lapply(from, coordinates), function(x) 
+                           do.call("rbind", lapply(x, function(z) 
+                             z[[1]]))))
+  
+  coords.df = cbind(coords.mat[seq(1, nrow(coords.mat), by = 2), ], 
+                    coords.mat[seq(2, nrow(coords.mat), by = 2), ])
+  
+  if (is.projected(from[[1]]))
+    colnames(coords.df) = c("x", "y", "x_end", "y_end")
+  else
+    colnames(coords.df) = c("lon", "lat", "lon_end", "lat_end")
+  
+  data.frame(cbind(fetch.df, coords.df))
+})
+
+
+#' @importFrom methods setAs slot
+#' @importFrom sp SpatialLines Lines proj4string CRS
+setAs("Fetch", "SpatialLines", function(from){
+  validObject(from)
+  
+  lines_list = do.call("c", lapply(from, slot, "lines"))
+  line_list = do.call("c", lapply(lines_list, slot, "Lines"))
+  lines_list = list(Lines(line_list, ID = "all_fetch_lines"))
+  
+  SpatialLines(lines_list, CRS(proj4string(from)))
+})
+
+#' Plot a Fetch Object
+#' 
+#' Plot method for \code{\link{Fetch}} objects.
+#' 
+#' These plot methods allow for the fetch vectors to be plotted (missing 
+#' \code{y}), or the fetch vectors to be plotted along with a 
+#' \code{\link[sp]{SpatialPolygons}} object. If both \code{x} and \code{y} are
+#' supplied, then it does not matter which argument is supplied first.
+#' 
+#' @param x either a \code{\link{Fetch}} object as returned by 
+#'          \code{\link{fetch}}, or a \code{\link[sp]{SpatialPolygons}} object.
+#' @param y can be missing, otherwise same as \code{x}.
+#' @param ... further arguments passed to 
+#'            \code{\link[sp]{plot,SpatialLines,missing-method}}.
+#' 
+#' @importFrom methods setMethod as
+#' @importFrom sp plot
+#' @export
+setMethod("plot",
+          signature(x = "Fetch", y = "missing"),
+          definition = function(x, y, ...){
+            validObject(x)
+            plot(as(x, "SpatialLines"), ...)
+          })
+
+#' @rdname plot-Fetch-missing-method
+#' @importFrom methods setMethod as signature validObject
+#' @importFrom sp plot proj4string CRS identicalCRS
+#' @export
+setMethod("plot",
+          signature(x = "Fetch", y = "SpatialPolygons"),
+          definition = function(x, y, ...){
+            validObject(x)
+            
+            identical_crs = identicalCRS(x, y)
+            
+            if (!identical_crs){
+              warning(paste("transforming fetch vectors onto the same map", 
+                            "CRS as the polygon layer"), call. = FALSE)
+              x = spTransform(x, CRS(proj4string(y)))
+            }
+
+            plot(as(x, "SpatialLines"), ...)
+            plot(y, add = TRUE, col = "lightgrey", border = NA)
+          })
+
+#' @rdname plot-Fetch-missing-method
+#' @importFrom methods setMethod signature validObject
+#' @export
+setMethod("plot",
+          signature(x = "SpatialPolygons", y = "Fetch"),
+          definition = function(x, y, ...){
+            plot(y, x, ...)
+          })
+
+#' Write a Fetch object to a KML file
+#' 
+#' Write a \code{\link{Fetch}} object to a KML file. Various aesthetics
+#' parameters can be set via \code{colour}, \code{alpha}, \code{size}, 
+#' \code{shape} arguments.
+#' 
+#' @param obj a \code{\link{Fetch}} object
+#' @param folder.name character; folder name in the KML file
+#' @param file.name character; output KML file name
+#' @param overwrite logical; overwrite the existing file if one exists? Default 
+#'                  FALSE
+#' @param ... additional aesthetics arguments passed to 
+#'            \code{\link[plotKML]{kml_layer.SpatialLines}}
+#' 
+#' @importFrom methods setMethod signature validObject
+#' @importFrom plotKML kml kml_open kml_close kml_layer normalizeFilename
+#' @export
+setMethod("kml",
+          signature(obj = "Fetch"),
+          definition = 
+            function(obj, 
+                     folder.name = normalizeFilename(deparse(
+                       substitute(obj, env = parent.frame()))), 
+                     file.name = paste(normalizeFilename(deparse(
+                       substitute(obj, env = parent.frame()))), ".kml", 
+                       sep = ""), 
+                     overwrite = FALSE, ...){
+            validObject(obj)
+            kml_open(file.name = file.name, folder.name = folder.name, 
+                     overwrite = overwrite, kml_visibility = FALSE)
+            
+            for (i in seq_along(obj))
+              kml_layer(obj[[i]], subfolder.name = obj@names[i], ...)
+            
+            kml_close(file.name)
+          })
---
+++ README.md
@@ -0,0 +1,101 @@
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+[![Build Status](https://travis-ci.org/blasee/fetchR.svg)](https://travis-ci.org/blasee/fetchR)
+
+Wind fetch
+==========
+
+Wind fetch is an important measurement in coastal applications. It provides a measurement for the unobstructed length of water over which wind from a certain direction can blow over. The higher the wind fetch from a certain direction, the more energy is imparted onto the surface of the water resulting in a larger sea state. Therefore, the larger the fetch, the larger the exposure to wind and the more likely the site experiences larger sea states.
+
+Why **fetchR**?
+===============
+
+Averaging the wind fetch for numerous directions at the same location is a reasonable measure of the overall wind exposure. This process of calculating wind fetch can be extremely time-consuming and tedious, particularly if a large number of fetch vectors are required at many locations. The **fetchR** package calculates wind fetch for any marine location on Earth. There are also plot methods to help visualise the wind exposure at the various locations, and methods to output the fetch vectors to a KML file for further investigation.
+
+Installation
+============
+
+You can install the latest version of **fetchR** from GitHub.
+
+``` r
+if (!require(devtools))
+  install.packages("devtools")
+
+devtools::install_github("blasee/fetchR", build_vignettes = TRUE)
+
+# Load the fetchR package
+library(fetchR)
+```
+
+Calculating wind fetch with **fetchR**
+======================================
+
+If you already have a `SpatialPolygons` object representing the coastline and surrounding islands, and a `SpatialPoints` object representing the locations, then calculating wind fetch with **fetchR** is easy. You can just pass these two arguments into the `fetch` function.
+
+![Map showing the projected SpatialPolygons (filled grey polygons) and SpatialPoints (black crosses) objects.](./vignettes/figures/proj.png)
+
+``` r
+# Calculate wind fetch by passing in the projected SpatialPolygons object (nz_poly_proj)
+# and the projected SpatialPoints object (fetch_locs_proj) to the fetch function.
+my_fetch_proj = fetch(nz_poly_proj, fetch_locs_proj)
+
+my_fetch_proj
+```
+
+##### Note
+
+The data for the polygon layer was sourced from [Land Information New Zealand](https://data.linz.govt.nz/layer/1153-nz-coastlines-and-islands-polygons-topo-150k/)[1].
+
+    #> Is projected : TRUE
+    #> Max distance : 300 km
+    #> Directions   : 36
+    #> Sites        : 3
+    #> 
+    #>                  North East South West Average
+    #> Kawau Bay          2.3  8.6   6.7  4.8     5.6
+    #> Chetwode Islands 158.9 75.4  12.1 17.1    65.9
+    #> Foveaux Strait    27.6 89.5 112.4 96.8    81.6
+
+The `my_fetch_proj` provides a summary of the fetch for all the four quadrants, along with an overall average of the fetch length at all the sites.
+
+Visualise the fetch vectors
+===========================
+
+``` r
+# Plot the fetch vectors, along with the coastline and surrounding islands
+plot(my_fetch_proj, nz_poly_proj)
+```
+
+![Fetch vectors at all the sites](./vignettes/figures/nz_fetch.png)
+
+![Fetch vectors at Kawau Bay](./vignettes/figures/kawau.png)
+
+![Fetch vectors at the Chetwode Islands](./vignettes/figures/chetwode.png)
+
+![Fetch vectors at Foveaux Strait](./vignettes/figures/foveaux.png)
+
+Export to a KML file
+====================
+
+``` r
+# Export the fetch vectors to a KML file for further investigation
+kml(my_fetch_proj)
+```
+
+Note that the distances calculated in Google Earth are (almost) the same as the distances calculated with **fetchR**. This can be seen in the KML output as the fetch vector at 90 degrees for the Foveaux Strait site is 300km (the maximum distance by default) in both **fetchR** and Google Earth, although these algorithms differ.
+
+![Output to KML](./vignettes/figures/kml.png)
+
+Get started with **fetchR**
+===========================
+
+Read the short introductory vignette to get you started with **fetchR**, and have a look at the simple, reproducible example in the `fetch` function.
+
+``` r
+# Read the short vignette
+vignette("introduction-to-fetchR")
+
+# Reproduce a simple example
+example(fetch)
+```
+
+[1] This README contains data that was sourced from Land Information New Zealand without adaptation that is protected under CC-By Land Information New Zealand.
